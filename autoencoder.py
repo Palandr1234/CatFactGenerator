587f0fd7ec673a64a004649793cc44e7926c5a2c
Palandr1234 <a.palaev@innopolis.university>
2021-04-13 22:19:58 +0300

    Architeture changed to Variational Autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -5,46 +5,43 @@ import torch.nn.functional as F
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# encoder part of the AutoEncoder
-class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(EncoderRNN, self).__init__()
-        self.hidden_size = hidden_size
+class VAE(nn.Module):
+    def __init__(self, input_size, args):
+        super(VAE, self).__init__()
+        self.embed = nn.Embedding(input_size, args.embed_size)
+        self.lstm1 = nn.LSTM(args.embed_size, args.hidden_size, args.nlayers, dropout=args.dropout, bidirectional=True)
+        self.dropout = nn.Dropout(args.dropout)
+        self.h2mu = nn.Linear(args.hidden_size * args.nlayers * 2, args.latent_dim)
+        self.h2logvar = nn.Linear(args.hidden_size * args.nlayers * 2, args.latent_dim)
+        self.z2emb = nn.Linear(args.latent_dim, args.embed_size)
+        self.lstm2 = nn.LSTM(args.embed_size, args.hidden_size, args.nlayers, dropout=args.dropout)
+        self.proj = nn.Linear(args.hidden_size, input_size)
+        self.max_length = args.max_length
 
-        self.embed = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
+    def encode(self, input):
+        input = self.embed(input).view(len(input), 1, -1)
+        _, (h, _) = self.lstm1(input)
+        h = self.dropout(h.view(1, -1))
+        return self.h2mu(h), self.h2logvar(h)
 
-    def forward(self, input, hidden):
-        embedded = self.embed(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.gru(output, hidden).view(1, 1, -1)
-        return output, hidden
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
 
-    def init_hidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+    def decode(self, z, hidden=None):
+        z = z.repeat(self.max_length, 1)
+        z = self.z2emb(z)
+        z = z.view(len(z), 1, -1)
+        output, _ = self.lstm2(z, hidden)
+        output = self.dropout(output).view(-1, output.size(-1))
+        output = self.proj(output)
+        return output
 
-
-# decoder part of the AutoEncoder
-class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-
-        self.embed = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        output = self.embed(input).view(1, 1, -1)
-
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))
-        return output, hidden
-
-    def init_hidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+    def forward(self, input):
+        mu, logvar = self.encode(input)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z)
 
 
 
