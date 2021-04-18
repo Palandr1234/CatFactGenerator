2f23e917ccb4eefeb71d3d1cf2af49791730d15a
Palandr1234 <a.palaev@innopolis.university>
2021-04-18 22:30:22 +0300

    initialization added

--- autoencoder.py
+++ autoencoder.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch
 
 
 class Model(nn.Module):
@@ -6,10 +7,10 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.hidden_size = 128
         self.embed_size = 128
-        self.nlayers = 3
+        self.n_layers = 3
         n_vocab = len(dataset.unique_words)
         self.embed = nn.Embedding(n_vocab, self.embed_size)
-        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size, self.nlayers, dropout=0.2)
+        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size, self.n_layers, dropout=0.2)
         self.fc = nn.Linear(self.hidden_size, n_vocab)
 
     def forward(self, x, prev_state):
@@ -17,3 +18,8 @@ class Model(nn.Module):
         output, state = self.lstm(embed, prev_state)
         logits = self.fc(output)
         return logits, state
+
+    def init_state(self, sequence_length):
+        return (torch.zeros(self.n_layers, sequence_length, self.hidden_size),
+                torch.zeros(self.n_layers, sequence_length, self.hidden_size))
+
