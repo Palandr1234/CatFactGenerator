a679485cc636c8cafcaf5f69deca97f43837d200
Palandr1234 <a.palaev@innopolis.university>
2021-04-20 23:32:15 +0300

    File renamed

--- model.py
+++ model.py
@@ -5,12 +5,12 @@ import torch
 class Model(nn.Module):
     def __init__(self, dataset):
         super(Model, self).__init__()
-        self.hidden_size = 128
-        self.embed_size = 128
-        self.n_layers = 3
+        self.hidden_size = 256
+        self.embed_size = 200
+        self.n_layers = 4
         n_vocab = len(dataset.unique_words)
         self.embed = nn.Embedding(n_vocab, self.embed_size)
-        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size, self.n_layers, dropout=0.2)
+        self.lstm = nn.LSTM(self.embed_size, self.hidden_size, self.n_layers, dropout=0.3)
         self.fc = nn.Linear(self.hidden_size, n_vocab)
 
     def forward(self, x, prev_state):
