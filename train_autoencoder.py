558717f2268aa5dcdd43d5795239216a3b177a0d
Palandr1234 <a.palaev@innopolis.university>
2021-04-21 00:17:13 +0300

    Everything working

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -7,7 +7,8 @@ import torch.nn as nn
 from torch import optim
 from torch.utils.data import DataLoader
 from preprocess_data import Dataset
-from autoencoder import Model
+import numpy as np
+from model import Model
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -17,7 +18,7 @@ def train(dataset, model, args):
 
     dataloader = DataLoader(dataset, batch_size=args.batch_size)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-2)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
     for epoch in tqdm(range(args.max_epoch)):
         state_h, state_c = model.init_state(args.sequence_length)
@@ -31,19 +32,26 @@ def train(dataset, model, args):
             state_c = state_c.detach()
 
             loss.backward()
+            nn.utils.clip_grad_norm_(model.parameters(), 1)
             optimizer.step()
             losses.append(loss.item())
         print({'epoch': epoch, 'loss': sum(losses)/len(losses)})
 
 
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument('--max_epoch', type=int, default=10)
-parser.add_argument('--batch_size', type=int, default=64)
-parser.add_argument('--sequence_length', type=int, default=4)
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--max_epoch', type=int, default=40)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--sequence_length', type=int, default=4)
+    parser.add_argument('--dataset_path', type=str, default='output/vocabulary.pkl')
+    parser.add_argument('--save_path', type=str, default='state_dict_model.pt')
+    args = parser.parse_args()
 
-with open('output/vocabulary.pkl', 'rb') as f:
-    dataset = pickle.load(f)
+    with open(args.dataset_path, 'rb') as f:
+        dataset = pickle.load(f)
 
-model = Model(dataset).to(device)
-train(dataset, model, args)
+    model = Model(dataset).to(device)
+    train(dataset, model, args)
+    PATH = args.save_path
+
+    torch.save(model.state_dict(), PATH)
