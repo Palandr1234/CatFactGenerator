a159f1bf6df573716f5266f07de588219a1a717a
Palandr1234 <a.palaev@innopolis.university>
2021-04-22 23:39:32 +0300

    Everything working

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from torch import nn
 import torch
+from torch.autograd import Variable
 
 
 class Model(nn.Module):
@@ -10,16 +11,16 @@ class Model(nn.Module):
         self.n_layers = 4
         n_vocab = len(dataset.unique_words)
         self.embed = nn.Embedding(n_vocab, self.embed_size)
-        self.lstm = nn.LSTM(self.embed_size, self.hidden_size, self.n_layers, dropout=0.3)
+        self.gru = nn.GRU(self.embed_size, self.hidden_size, self.n_layers)
+        self.dropout = nn.Dropout(0.3)
         self.fc = nn.Linear(self.hidden_size, n_vocab)
 
     def forward(self, x, prev_state):
         embed = self.embed(x)
-        output, state = self.lstm(embed, prev_state)
+        output, state = self.gru(embed, prev_state)
+        output = self.dropout(output)
         logits = self.fc(output)
         return logits, state
 
     def init_state(self, sequence_length):
-        return (torch.zeros(self.n_layers, sequence_length, self.hidden_size),
-                torch.zeros(self.n_layers, sequence_length, self.hidden_size))
-
+        return Variable(torch.zeros(self.n_layers, sequence_length, self.hidden_size))
