31f9023e1b2fd83d3aef9f521e9a985e2b7e6b05
Palandr1234 <a.palaev@innopolis.university>
2021-05-30 21:14:24 +0300

    Comments added

--- test.py
+++ test.py
@@ -7,26 +7,36 @@ from torch.utils.data import DataLoader
 import argparse
 
 
+# given the beginning, generate the text
 def predict(dataset, model, text, next_words=100):
+    # normalize the given beginning
     words = normalizeString(text).split()
     model.eval()
     state = model.init_state(len(words))
 
     for i in range(0, next_words):
+        # convert the text into a tensor
         x = torch.tensor([[dataset.word2idx[w] for w in words[i:]]])
+        # predict the next word
         y_pred, state = model(x, state)
 
         last_word_logits = y_pred[0][-1]
+        # get the probabilities of each word to be next
         p = torch.nn.functional.softmax(last_word_logits, dim=0).detach().numpy().astype('float64')
-        idx = p.argsort()[-5:][::-1]
+        # choose 3 most probable words
+        idx = p.argsort()[-3:][::-1]
+        # get probabilities of these words
         new_p = np.zeros(p.shape).astype('float64')
+        # normalize these probabilities
         new_p[idx] = (p[idx]/np.sum(p[idx]))
-        # new_p[idx] /= np.sum(new_p)
+        # choose the next word using these probabilities
         word_index = np.random.choice(len(last_word_logits), p=new_p)
+        # if we get the end of the text then we need to stop generating
         if dataset.idx2word[word_index] == "EOS":
             break
+        # add generated word to the list
         words.append(dataset.idx2word[word_index])
-
+    # return list of generated words
     return words
 
 
@@ -38,13 +48,13 @@ if __name__ == '__main__':
     parser.add_argument('--input', type=str, default="the")
 
     args = parser.parse_args()
+    # load the dataset
     with open(args.dataset, 'rb') as f:
         dataset = pickle.load(f)
     PATH = args.model
+    # load the model
     model = Model(dataset)
     model.load_state_dict(torch.load(PATH))
     model.eval()
+    # generate the text for the given beginning
     print(' '.join(predict(dataset, model, text=args.input)))
-
-
-
